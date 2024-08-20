6a8f170c394e0113d6abcfdb2e9af2c3805d1c1a
dariobriganti <54713838+dariobriganti@users.noreply.github.com>
2024-08-20 00:22:09 -0300

    Update script.js
    Filtrar por nubosidad 0%-20%

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = 'be9b9f7295d5f005696dec5b3f7d1d63';
     const polygonId = '66c355ac641959eff0d662f8';
+    const cloud_min = '0';
+    const cloud_max = '20';
 
     document.getElementById('fetch-data').addEventListener('click', function() {
         const startDateInput = document.getElementById('start-date').value;
@@ -10,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const startDate = new Date(startDateInput).getTime() / 1000; // Convertir a timestamp UNIX
             const endDate = new Date(endDateInput).getTime() / 1000; // Convertir a timestamp UNIX
 
-            const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}`;
+            const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}&clouds=${clouds_min},${clouds_max}`;
 
             fetch(url)
                 .then(response => response.json())
