9fac78aac13be21959791a0f194ac8a3b2947b19
dariobriganti <54713838+dariobriganti@users.noreply.github.com>
2024-08-20 02:12:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = 'be9b9f7295d5f005696dec5b3f7d1d63';
     const polygonId = '66c355ac641959eff0d662f8';
+    const cloudMax = '20';
 
     document.getElementById('fetch-data').addEventListener('click', function() {
         const startDateInput = document.getElementById('start-date').value;
@@ -10,12 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const startDate = new Date(startDateInput).getTime() / 1000; // Convertir a timestamp UNIX
             const endDate = new Date(endDateInput).getTime() / 1000; // Convertir a timestamp UNIX
 
-            const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}`;
+            const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}&clouds_max=${cloudsMax}`;
 
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data); // Verifica la estructura de los datos
                     const ndviContainer = document.getElementById('ndvi-value');
                     ndviContainer.innerHTML = ''; // Limpiar el contenido previo
 
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
                         data.forEach(record => {
                             const ndviValue = record.data.mean.toFixed(2); // Redondear a dos decimales
-                            const cloudiness = record.clouds ? record.clouds.toFixed(2) : 'N/A'; // Extraer nubosidad, si está disponible
                             const date = new Date(record.dt * 1000); // Convierte la fecha a formato legible
                             const formattedDate = date.toLocaleDateString('es-ES', {
                                 year: 'numeric',
@@ -35,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                             // Crear y agregar un nuevo elemento para cada registro
                             const recordElement = document.createElement('p');
-                            recordElement.textContent = `Valor de NDVI: ${ndviValue} (Nubosidad: ${cloudiness}%) (Fecha: ${formattedDate})`;
+                            recordElement.textContent = `Valor de NDVI: ${ndviValue} (Fecha: ${formattedDate})`;
                             ndviContainer.appendChild(recordElement);
                         });
                     } else {
