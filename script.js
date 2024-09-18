b1d3fa9190c130f24cfc94a9e5743e55035764d5
dariobriganti <54713838+dariobriganti@users.noreply.github.com>
2024-09-18 11:31:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
             const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}&clouds_max=${maxClouds}`;
 
             fetch(url)
+                console.log('URL:', url);
+
                 .then(response => response.json())
                 .then(data => {
                     const ndviContainer = document.getElementById('ndvi-value');
