b7a043f2b12938ecb9c8c1b4b241161204094a18
dariobriganti <54713838+dariobriganti@users.noreply.github.com>
2024-09-18 11:39:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const endDate = new Date(endDateInput).getTime() / 1000; // Convertir a timestamp UNIX
 
             const url = `https://api.agromonitoring.com/agro/1.0/ndvi/history?start=${startDate}&end=${endDate}&polygon_id=${polygonId}&appid=${apiKey}&clouds_max=${maxClouds}`;
+            console.log('URL:', url);
 
             fetch(url)
                 .then(response => response.json())
